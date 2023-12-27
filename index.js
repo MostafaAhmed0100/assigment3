8a5e14032b12eca0a2c174fb176daec6f95b43a8
MostafaAhmed0100 <154384461+MostafaAhmed0100@users.noreply.github.com>
2023-12-27 05:53:53 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,227 @@
+
+
+
+/* (1) var num= Number(window.prompt("enter your any number"));
+
+console.log(num); */
+
+
+
+/* (2) var num = Number(window.prompt("enter your any number"));
+
+if(num % 3==0 && num % 4==0){
+    console.log("yes");
+}
+
+else{
+    console.log("no");
+}
+ */
+
+
+
+/* (3) var num1 = Number(window.prompt("enter your number1"));
+
+var num2 = Number(window.prompt("enter your number2"));
+
+if(num1 > num2){
+    console.log(num1);
+}else{
+    console.log(num2);
+}
+ */
+
+
+
+/* (4) var num = Number(window.prompt("enter your any number"));
+
+if(num%2==0){
+    console.log("positive number");
+}else{
+    console.log("negative number");
+} */
+
+
+/* (5) var num1 = Number(window.prompt("enter your number1"));
+var num2 = Number(window.prompt("enter your number2"));
+var num3 = Number(window.prompt("enter your number3"));
+
+if(num1>num2 && num1>num3){
+    console.log("max element =" + num1);
+}else if(num2>num1 && num2>num3){
+    console.log("max element =" + num2);
+}else{
+    console.log("max element =" + num3); 
+}
+
+if(num1<num2 && num1<num3){
+    console.log("min element=" + num1);
+}else if(num2<num1 && num2<num3){
+    console.log("min element=" + num2);
+}else{
+    console.log("min element=" + num3); 
+} */
+
+
+
+
+/* (6) var num = Number(window.prompt("enter your number"));
+
+if(num%2==0){
+    console.log("even");
+
+}else{
+    console.log("odd");
+} */
+
+
+
+/* (9) var num = Number(window.prompt("enter your ineger number"));
+
+for(var num=1; num<=5; num++){
+    console.log(num);
+} */
+
+
+ /* (10) var num = Number(window.prompt('enter your any integer number'));
+
+for(var i=1; i<=12; i++ )
+{
+   console.log(i*1);
+} 
+ */
+
+
+/* (11) var num = Number(window.prompt("enter your any even number"));
+
+for(var num=0; num<=14; num+=2){
+ console.log(num);
+} */
+
+
+
+/* (12) var num1= Number(window.prompt('enter your number1'));
+var num2= Number(window.prompt('enter your number2'));
+
+ reuslt= num1 * num2 * num2;
+
+console.log(reuslt); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* (14) var mark = Number(window.prompt("Enter the physics degree "));
+
+var mark = Number(window.prompt("Enter the chemistry degree "));
+
+var mark = Number(window.prompt("Enter the biology degree "));
+
+var mark = Number(window.prompt("Enter the mathematics degree "));
+
+var mark = Number(window.prompt("Enter the computer degree "));
+
+if(mark>90 && mark<=100){
+    console.log("grad A");
+}else if(mark>80 && mark<=90){
+    console.log("grad B");
+
+}else if(mark>70 && mark<=80){
+    console.log("grad C");
+
+}else if(mark>60 && mark<=70){
+    console.log("grad D");
+}else if(mark>=40){
+    console.log("grad E");
+}else {
+    console.log("grad f");
+} */
+
+
+/* (17) var num1 = Number(window.prompt("any your any number"));
+
+var num2 = Number(window.prompt("enter your any number"));
+
+switch (num1 > num2) {
+    case true:
+        console.log(' max element=' + "num1");
+        break;
+
+    default:
+        console.log(' max element=' + "num2");
+} */
+
+
+
+
+/* (18) var num = Number(window.prompt("enter your any number"));
+
+switch(num %3==0){
+    case true:
+        console.log("even");
+        break;
+    default :
+        console.log("odd");
+
+} */
+
+
+
+
+
+/* (19) var num = Number(window.prompt("enter your any number"));
+
+switch(num %2==0){
+    case true :
+        console.log("positive");
+        break;
+    default :
+        console.log("negative");
+}
+ */
+
+
+
+
+
+
+
+
+
+
+/* (20) var num1 = Number(window.prompt("enter your number1"));
+
+var num2 =Number(window.prompt("enter your number2"));
+
+var title =window.prompt("enter your title");
+
+switch(title){
+    case '+' :
+        console.log(num1 + num2);
+        break;
+    case  '-' :
+        console.log(num1 - num2);
+        break;
+    case '/' :
+    console.log(num1 / num2);
+        break;
+
+    case '*' :
+    console.log( num1* num2);
+
+    break;
+
+    default :
+    console.log("no number");
+        
+
+}  */
